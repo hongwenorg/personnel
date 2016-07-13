9f432e9007f5104506b2462a6fe20cfc823362b2
wenjiabao <wen8555955@163.com>
2016-07-13 14:11:45 +0800

    000

--- Application/Home/Controller/PropertyController.class.php
+++ Application/Home/Controller/PropertyController.class.php
@@ -2,7 +2,7 @@
 /**
 *@Author:温加宝
 *@Email:wen8555955@163.com
-*@DateTime:2016/3/17
+*@DateTime:2016/7/13
 *@Description:固定资产系统
 */
 namespace Home\Controller;
@@ -11,7 +11,7 @@ header('P3P: CP="ALL ADM DEV PSAi COM OUR OTRo STP IND ONL"');
 header("Content-type:text/html;charset=utf-8");
 header('Access-Control-Allow-Origin: *');
 require_once( "/Public/public.php" );
-class ProertyController extends CommonController {
+class PropertyController extends CommonController {
 	function purchase(){
 		$this->display();
 	}
