53a1366ac5217eeed839b206dbb4449696de1c2c
wenjiabao <wen8555955@163.com>
2016-07-16 16:10:33 +0800

    aaaaaaa

--- Application/Home/Controller/PropertyController.class.php
+++ Application/Home/Controller/PropertyController.class.php
@@ -15,6 +15,60 @@ class PropertyController extends CommonController {
 	function purchase(){
 		$this->display();
 	}
+
+	//计划申请页面信息
+	function plan_list(){
+		echo 1;die;
+		$data_arr = array();
+		$data_arr[] = $this->school_sel();
+		$data_arr[] = $this->plan_sel();
+		$data_arr[] = $this->purchase();
+		$data_arr[] = $this->finance_type();
+
+		/*$oa_apply = D("oa_apply");
+		$arr = array();
+		$arr = $oa_apply->where("is_del!=1")->select();
+		//echo "<pre>";
+		//print_r($class_arr);*/
+		echo json_encode($data_arr);
+	}
+
+	//校区的下拉列表数据
+	function school_sel(){
+		$model = D('campus_class_post');
+		$array = array();
+		$array = $model->where(array('status'=>1,'type'=>1))->select();
+		return $array;
+	}
+
+
+	//计划类型的数据
+	function plan_sel(){
+		$model = D('property_plan');
+		$array = array();
+		$array = $model->select();
+		return $array;
+	}
+
+
+	//科目类别的数据
+	function purchase(){
+		$model = D('property_purchase');
+		$array = array();
+		$array = $model->select();
+		return $array;
+	}
+
+
+	//采购类型
+	function finance_type(){
+		$model = D('property_finance_type');
+		$array = array();
+		$array = $model->where(array('is_del'=>0))->select();
+		return $array;
+	}
+
+
 }
 
 ?>
